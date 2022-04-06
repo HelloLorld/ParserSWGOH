ebce87a51ca5220079b45c7181a22dbddc87bff1
Ruslan <danilov2001@mail.ru>
2022-04-06 21:14:02 +0300

    возможно конечный вариант

--- main.py
+++ main.py
@@ -98,9 +98,11 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon('ico.ico'))
     MainWindow = QtWidgets.QMainWindow()
+    global ui
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    ui.pushButton.clicked.connect(swCall)
     sys.exit(app.exec_())
 
 
